c7da9f8ea634d43820e9f4c2d7e00561ec10ad34
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-11-18 10:48:28 -0600

    Update MarkedActorSheet.mjs

--- scripts/actor/MarkedActorSheet.mjs
+++ scripts/actor/MarkedActorSheet.mjs
@@ -1,1 +1,22 @@
+// scripts/actor/MarkedActorSheet.mjs
+
+export class MarkedActorSheet extends ActorSheet {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["marked", "sheet", "actor"],
+      template: "systems/marked-system/templates/actors/actor-sheet.hbs",
+      width: 900,
+      height: 700,
+      tabs: [
+        { navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "main" }
+      ]
+    });
+  }
+
+  getData(options) {
+    const data = super.getData(options);
+    data.config = game.marked?.config;
+    return data;
+  }
+}
 
