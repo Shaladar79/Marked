ba487447b12714fab76427fc079bb4f3aeb8a9e3
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-11-21 09:57:13 -0600

    Update MarkedActorSheet.mjs

--- scripts/actor/MarkedActorSheet.mjs
+++ scripts/actor/MarkedActorSheet.mjs
@@ -4,33 +4,34 @@ import { MarkedConfig } from "../config.mjs";
 
 export class MarkedActorSheet extends ActorSheet {
   static get defaultOptions() {
-  return foundry.utils.mergeObject(super.defaultOptions, {
-    classes: ["marked", "sheet", "actor"],
-    template: "systems/the-marked-system/templates/actors/actor-sheet.hbs",
-    width: 900,
-    height: 700,
-    tabs: [
-      { navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "main" }
-    ],
-    submitOnChange: true,
-    submitOnClose: true
-  });
-}
-
-
-  getData(options) {
-    const data = super.getData(options);
-
-    // Expose our config so templates can access it via {{config}}
-    data.config = MarkedConfig;
-
-    return data;
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["marked", "sheet", "actor"],
+      template: "systems/the-marked-system/templates/actors/actor-sheet.hbs",
+      width: 900,
+      height: 700,
+      tabs: [
+        { navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "main" }
+      ],
+      submitOnChange: true,
+      submitOnClose: true
+    });
   }
 
-activateListeners(html) {
-    super.activateListeners(html);
+  getData(options) {
+    const context = super.getData(options);
 
-    // You may add custom listeners later here
-    // console.log("Sheet listeners active");
+    // 🔹 This is the important bit:
+    // Foundry puts your document data under `data`,
+    // so copy `data.system` to top-level `system`
+    context.system = context.data.system;
+
+    // You already had this:
+    context.config = MarkedConfig;
+
+    return context;
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html);
   }
 }
